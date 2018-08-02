d8fbf10e650e8e41842c005d760350c3e0d18f03
陈志同 <chenzhitong12@hotmail.com>
2018-08-02 16:07:59 +0800

    initial commit

---
+++ ImageCleaner/Context.cs
@@ -0,0 +1,32 @@
+﻿using ImageCleaner.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCleaner
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base()
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = (string)JObject.Parse(File.ReadAllText("appsettings.json"))["ConnectionStrings"]["DefaultConnection"];
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+
+}
---
+++ ImageCleaner/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ImageCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles("../upload");
+            
+            using (var context = new Context())
+            {
+                var blogImgs = string.Join(' ', context.Blogs.Select(p => p.Content));
+                var eventImgs = string.Join(' ', context.Events.Select(p => p.Details));
+                var eventCovers = string.Join(' ', context.Events.Select(p => p.Cover));
+                foreach (var file in files)
+                {
+                    var img = file.Split('\\').Reverse().ToArray()[0];
+                    if (blogImgs.Contains(img)) continue;
+                    if (eventImgs.Contains(img)) continue;
+                    if (eventCovers.Contains(img)) continue;
+                    File.Delete(file);
+                    Console.WriteLine($"已删除{img}");
+                }
+            }
+            Console.WriteLine($"已完成");
+            Console.ReadLine();
+        }
+    }
+}
